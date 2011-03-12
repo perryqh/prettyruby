02bb4e4a1db1efbfaa7f40e1cfba17285ce15742
Perry Hertler <perry@hertler.org>
2011-03-11 21:09:39 -0600

    Putting all code in one file

--- prettyruby.py
+++ prettyruby.py
@@ -5,7 +5,7 @@ class PrettyRubyCommand(sublime_plugin.TextCommand):
 		view = self.view
 		region = sublime.Region(0L, view.size())
 
-		reformatter = rubyformatter.RubyFormatter(view.substr(region))
+		reformatter = RubyFormatter(view.substr(region))
 		beautified = reformatter.run()
 
 		view.replace(edit, region, beautified) 
